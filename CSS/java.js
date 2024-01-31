dd8a916c23bfb3f36552e0ca466067a4cdb4754e
Daisy Arequipa <daisy.arequipa@outlook.es>
2024-01-30 21:42:20 -0500

    reproductor

---
+++ CSS/java.js
@@ -0,0 +1,11 @@
+var numero = prompt("Ingrese un número:");
+
+numero = parseFloat(numero);
+
+if (numero > 0) {
+    alert("El número es positivo.");
+} else if (numero < 0) {
+    alert("El número es negativo.");
+} else {
+    alert("El número es cero.");
+}
---
+++ clasemartes.js
@@ -0,0 +1,6 @@
+const tarea = ["1", 'hola', 66, false, "chau", "auto"]
+for (let i = 0; i < tarea.length; i++) {
+    if (typeof tarea [i] === 'number' ) 
+    numeros.push(tarea[i]);
+}
+console.log(tarea[i])
